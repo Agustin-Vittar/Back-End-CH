07179d060e80608cda50aaab20bec57a3abe424f
Agustin-Vittar <josevittar99@gmail.com>
2023-08-15 23:54:04 -0300

    Preentrega Nº 3 / Mialer & Twilio

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from "express";
 import handlebars from "express-handlebars";
 import session from "express-session";
 import MongoStore from "connect-mongo";
+import dotenv from "dotenv";
 import { ProductManager } from "./src/dao/productManager.js";
 import { cartsRouter } from "./src/routes/carts.router.js";
 import { productRouter } from "./src/routes/products.router.js";
@@ -14,32 +15,28 @@ import { connectMongo } from "./src/utils.js";
 import { MsgModel } from "./src/dao/models/msgs.model.js";
 import { iniPassport } from "./src/config/passport.config.js";
 import passport from "passport";
-
-<<<<<<< HEAD
-const app = express();
-const port = 8080;
-=======
+import nodemailer from "nodemailer";
+import twilio from "twilio";
 import { entorno } from "./src/config/env-config.js";
 
 import { productController } from "./src/routes/productsRealtime.router.js";
 
 //console.log(entorno);
-
+dotenv.config();
 const app = express();
 const port = entorno.PORT;
->>>>>>> 9460772 (Preentrega Nº 3)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+console.log({
+  user: process.env.GOOGLE_EMAIL,
+  pass: process.env.GOOGLE_PASSWORD,
+});
 
 app.use(
   session({
     store: MongoStore.create({
-<<<<<<< HEAD
-      mongoUrl:
-        "mongodb+srv://joseagusvittar:uASeHnVhyUW27POM@vittar-cluster.ddxfzsa.mongodb.net/ecommerce?retryWrites=true&w=majority",
-=======
       mongoUrl: entorno.MONGO_URL,
->>>>>>> 9460772 (Preentrega Nº 3)
       ttl: 86400 * 7,
     }),
     secret: "secret-pass",
@@ -48,17 +45,60 @@ app.use(
   })
 );
 
+// Mailer
+
+const transport = nodemailer.createTransport({
+  service: "gmail",
+  port: 587,
+  auth: {
+    user: process.env.GOOGLE_EMAIL,
+    pass: process.env.GOOGLE_PASSWORD,
+  },
+});
+
+app.get("/mail", async (req, res) => {
+  const result = await transport.sendMail({
+    from: process.env.GOOGLE_EMAIL,
+    to: "agusjose122012@hotmail.com",
+    subject: "Test email 2",
+    html: `
+      <div>
+        <h1>Test Email</h1>
+        <p>Prueba de enviar email</p>
+      </div>
+    `,
+  });
+  res.send({ status: "Success", result: "Email Sent" });
+});
+
+// Twilio
+
+const client = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
+app.get("/sms", async (req, res) => {
+  const result = await client.messages.create({
+    from: process.env.TWILIO_SMS_NUMBER,
+    to: "+543731438314",
+    body: "Buenassss",
+  });
+
+  res.send({ status: "Success", result: "SMS Sent" });
+});
+
 // Passport
 
 iniPassport();
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Mongo
+// Mongo
 
 connectMongo();
 
-//Config Handlebars
+// Config Handlebars
 app.engine("handlebars", handlebars.engine());
 app.set("views", __dirname + "/views");
 app.set("view engine", "handlebars");
